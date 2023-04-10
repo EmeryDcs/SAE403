79a97ba05d82dd96d378f533201eaba712c9389a
EmeryDcs <emery.descours@gmail.com>
2023-04-10 20:31:23 +0200

    UPD_ajoutEmery

--- src/Controller/AddprojetController.php
+++ src/Controller/AddprojetController.php
@@ -3,6 +3,7 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,6 +11,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Projet;
 use App\Entity\Commentaire;
 use App\Form\ProjetFormType;
@@ -18,7 +20,7 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 class AddprojetController extends AbstractController
 {
     public function formProjet(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger){
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
+        // $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
         $user = $this->getUser();
         
         $projet = new Projet(); //On crée un objet image qui prendra les valeurs du formulaire
@@ -64,5 +66,44 @@ class AddprojetController extends AbstractController
                 'form' => $form->createView(),
             ]
         );
+    }
+
+    public function addProjet(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response{
+        $projet = new Projet();
+
+        $data = json_decode($request->getContent(), true);
+
+        $imageFile = $data['photo']; //On récupère la variable url du formulaire
+        /** @var UploadedFile $brochureFile */
+        if ($imageFile) {
+            $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+            $safeFilename = $slugger->slug($originalFilename); 
+            //(pour renommer le fichier, utilise slug
+            $newFilename = $originalFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+            // le move_uploaded_files()
+            try {
+                $imageFile->move(
+                    $this->getParameter('image_directory'),
+                    $newFilename
+                );
+            } catch (FileException $e) {
+                // ...gérer les pbs d'upload ici
+            }
+            // stocker le nom de l'image dans le champ que l'on vient de créer
+            $projet->setPhoto($newFilename);
+        }
+
+        $projet->setAcquis($data['acquis']);
+        $projet->setAnnee($data['annee']);
+        $projet->setDescription($data['description']);
+        $projet->setDomaine($data['domaine']);
+        $projet->setNom($data['nom']);
+        $projet->setUtilisateur($data['user']); //à changer par l'utilisateur courant
+
+        if ($data['video']){
+            $projet->setVideo($data['acquis']);
+        }
+
+        return new Response('projet bien cree');
     }
 }
--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Entity\Commentaire;
 use App\Entity\Projet;
@@ -23,6 +25,24 @@ class CommentaireController extends AbstractController
         return new JsonResponse($result);
 
 
+    }
+
+    public function addCommentaire(Request $request, EntityManagerInterface $entityManager, ManagerRegistry $doctrine): Response{
+        $commentaire = new Commentaire();
+        $data = json_decode($request->getContent(), true);
+        $commentaire->setNote($data['note']);
+
+        $repository=$doctrine->getRepository(Projet::class);
+        $projet = $repository->findOneBy($data['id_projet']);
+        $commentaire->setProjet($projet);
+
+        $commentaire->setTexte($data['texte']);
+        $commentaire->setUtilisateur($data['id_user']); //à changer par l'utilisateur courant du coup
+        
+        $entityManager->persist($commentaire);
+        $entityManager->flush();
+
+        return new Response('Commentaire ajoute');
     }
 }
 
